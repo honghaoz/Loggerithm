908eb0ee70a9a0c40abba57f2ce066adbeacf806
Honghao Zhang <zhh358@gmail.com>
2015-08-14 17:38:54 -0400

    Added framework resource files Updated to 1.2 version

---
+++ Source/OSX Frameworkd Resource/Loggerithm.h
@@ -0,0 +1,19 @@
+//
+//  Loggerithm.h
+//  Loggerithm
+//
+//  Created by Honghao Zhang on 2015-08-14.
+//  Copyright (c) 2015 Honghao Zhang. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Loggerithm.
+FOUNDATION_EXPORT double LoggerithmVersionNumber;
+
+//! Project version string for Loggerithm.
+FOUNDATION_EXPORT const unsigned char LoggerithmVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Loggerithm/PublicHeader.h>
+
+
