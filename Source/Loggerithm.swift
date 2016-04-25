6b53fa875b1dce616a0c5ca5057e875dfee4deca
Honghao Zhang <zhh358@gmail.com>
2016-04-25 00:42:21 -0400

    Fixed

--- Source/Loggerithm.swift
+++ Source/Loggerithm.swift
@@ -158,7 +158,7 @@ public struct Loggerithm {
     
     - returns: The string logged out.
     */
-    public func verbose(format: String = "", function: String = #function, file: String = #file, line: Int = #line, args: CVarArg...) -> String? {
+    public func verbose(format: String = "", function: String = #function, file: String = #file, line: Int = #line, args: CVarArgType...) -> String? {
         if .Verbose >= logLevel {
             return log(.Verbose, function: function, file: file,  line: line, format: format, args: args)
         }
@@ -190,7 +190,7 @@ public struct Loggerithm {
     
     - returns: The string logged out.
     */
-    public func debug(format: String = "", function: String = #function, file: String = #file, line: Int = #line, args: CVarArg...) -> String?
+    public func debug(format: String = "", function: String = #function, file: String = #file, line: Int = #line, args: CVarArgType...) -> String?
     {
         if .Debug >= logLevel {
             return log(.Debug, function: function, file: file, line: line, format: format, args: args)
@@ -223,7 +223,7 @@ public struct Loggerithm {
     
     - returns: The string logged out.
     */
-    public func info(format: String = "", function: String = #function, file: String = #file, line: Int = #line, args: CVarArg...) -> String?
+    public func info(format: String = "", function: String = #function, file: String = #file, line: Int = #line, args: CVarArgType...) -> String?
     {
         if .Info >= logLevel {
             return log(.Info, function: function, file: file, line: line, format: format, args: args)
@@ -256,7 +256,7 @@ public struct Loggerithm {
     
     - returns: The string logged out.
     */
-    public func warning(format: String = "", function: String = #function, file: String = #file, line: Int = #line, args: CVarArg...) -> String?
+    public func warning(format: String = "", function: String = #function, file: String = #file, line: Int = #line, args: CVarArgType...) -> String?
     {
         if .Warning >= logLevel {
             return log(.Warning, function: function, file: file, line: line, format: format, args: args)
@@ -289,7 +289,7 @@ public struct Loggerithm {
     
     - returns: The string logged out.
     */
-    public func error(format: String = "", function: String = #function, file: String = #file, line: Int = #line, args: CVarArg...) -> String?
+    public func error(format: String = "", function: String = #function, file: String = #file, line: Int = #line, args: CVarArgType...) -> String?
     {
         if .Error >= logLevel {
             return log(.Error, function: function, file: file, line: line, format: format, args: args)
@@ -309,7 +309,7 @@ public struct Loggerithm {
     
     - returns: The string logged out.
     */
-    public func logWithLevel(level: LogLevel, _ format: String = "", function: String = #function, file: String = #file, line: Int = #line, args: CVarArg...) -> String?
+    public func logWithLevel(level: LogLevel, _ format: String = "", function: String = #function, file: String = #file, line: Int = #line, args: CVarArgType...) -> String?
     {
         if level >= logLevel {
             return log(level, file: file, function: function, line: line, format: format, args: args)
@@ -329,7 +329,7 @@ public struct Loggerithm {
     
     - returns: The string logged out.
     */
-    private func log(level: LogLevel, function: String = #function, file: String = #file, line: Int = #line, format: String, args: [CVarArg]) -> String
+    private func log(level: LogLevel, function: String = #function, file: String = #file, line: Int = #line, format: String, args: [CVarArgType]) -> String
     {
         let dateTime = showDateTime ? (UsingNSLog ? "" : "\(dateFormatter.stringFromDate(NSDate())) ") : ""
         let levelString = showLogLevel ? "[\(LogLevel.descritionForLogLevel(level))] " : ""
